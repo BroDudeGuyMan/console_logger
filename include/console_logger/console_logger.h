9a134594b66a9d31e91d0714e9de951a593c0664
Cayden Lawrence <hotsauce365247@gmail.com>
2026-03-02 19:55:03 -0500

    fixing docs

--- include/console_logger/console_logger.h
+++ include/console_logger/console_logger.h
@@ -5,37 +5,31 @@
 namespace logger {
 	/**
 	 * Optional function to make sure Windows terminals will render colors. Colors are enabled by default, but if the environment has them completely disabled, ANSI codes will render as gibberish.
-	 * @ingroup console
 	 */
 	void init();
 	/**
 	 * For simple messages and notifications.
 	 * @param[in] msg `std::string` to be displayed to the user.
-	 * @ingroup console
 	 */
 	void    info(std::string& msg);
 	/**
 	 * For prompting the user for input. Adds '>>>' to let the user know its a pompt.
 	 * @param[in] msg `std::string` to be displayed to the user.
-	 * @ingroup console
 	 */
 	void   input(std::string& msg);
 	/**
 	 * For letting the user know about possible issues.
 	 * @param[in] msg either an `e.what()` or a custom warning.
-	 * @ingroup console
 	 */
 	void warning(std::string& msg);
 	/**
 	 * For showing the user system error messages that will impact functionality.
 	 * @param[in] msg either an `e.what()` or a custom error message.
-	 * @ingroup console
 	 */
 	void   error(std::string& msg);
 	/** 
 	 * For alerting to an error that will completely halt the program or cause destructive behavior.
 	 * @param[in] msg either `e.what()` or a custom error message.
-	 * @ingroup console
 	 */
 	void   fatal(std::string& msg);
 }
